c7fd14128c7e2c6f3518575495858d6eaf34cd04
shankeleven <shashanksati11@gmail.com>
2025-07-18 05:13:29 +0530

    config parsing added

--- cmd/student-api/main.go
+++ cmd/student-api/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+
+
+// some structure basics:
+/*
+config usually has two config files dev and prod for obvious usecases
+we also would have to serialise config as some golang struct for it to be used inside the project by go program  s
+internals or pkg in projects is for the packages that are internally used i.e, inside the project and not exposed as such
+we had virt-launcher for example
+
+*/
+
+
 func main(){
 	fmt.Println("working fine")
 }
---
+++ internal/config/config.go
@@ -0,0 +1,62 @@
+package config
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+// we do define the structs in accordance with the config file , but how do we serialise/deserialise it?
+// could do it manually by reading the file
+// or use a package "cleanenv" , we are using those `` (struct tags) because of these
+ 
+type HTTPServer struct{
+	Addr string 
+ }
+
+
+
+ 
+type Config struct{
+	Env string `yaml:"env" env:"ENV" env-required:"true" env-default:"production"`// to use in other packages
+ StoragePath string `yaml:"storage_path" env-required:"true"`
+ HTTPServer `yaml:"http_server" `
+
+}
+
+func MustLoad() *Config{ // name to speak for itself
+	// for the functions that are must(in any context of getting the project up and running)
+	//  we don't keep error in return type as if they fail , we directly throw an error
+	var configPath string
+	configPath = os.Getenv(configPath)
+
+	if configPath==""{
+		flags:= flag.String("config","","path to teh configuration file")
+		flag.Parse()
+
+		configPath= *flags// dereferencing
+
+		
+	}
+
+	if configPath==""{
+		log.Fatal("config not mentioned") // for config not provided in either of those
+	}	else{
+
+	if _, err:= os.Stat(configPath);os.IsNotExist(err){
+			log.Fatal("configFile does not exist")
+	}
+}  // khachhar kaam for error handling if config path is wrong or the file is missing
+
+var cfg 	Config
+
+err:= cleanenv.ReadConfig(configPath, &cfg) 
+if(err!=nil){
+	log.Fatal("cannot read config file due to ", err.Error())// this error function is that errorf() from the error interface
+}
+ 
+return &cfg // cfg would be exported
+
+}
